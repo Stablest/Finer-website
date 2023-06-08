d3cbbcc69bf7c8b68b9ee949319531948e0cbc24
Stablest <marlonlocci@hotmail.com>
2023-06-08 03:11:05 -0300

    feature: Added default text color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-white`}>
+      <body className={`${inter.className} bg-platinum text-black-olive`}>
         <Header></Header>
         {children}
       </body>
