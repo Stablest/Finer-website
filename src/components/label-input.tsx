5a7fd188ab07b7a414cddebc66513a284973f9a1
Stablest <marlonlocci@hotmail.com>
2023-09-13 19:03:47 -0300

    refactor: Changed div border color

--- src/components/label-input.tsx
+++ src/components/label-input.tsx
@@ -11,6 +11,7 @@ type SizeLabelProps = {
 type LabelInputHTMLAttributes = Omit<LabelHTMLAttributes<HTMLLabelElement>, 'htmlFor'>
 
 export function LabelInput({ children, id, type, size, ...labelAttributes }: SizeLabelProps & LabelInputHTMLAttributes) {
+
     const newBorderRight = () => {
         if (size)
             return 'border-r-[1px] border-solid'
@@ -22,7 +23,7 @@ export function LabelInput({ children, id, type, size, ...labelAttributes }: Siz
             <div className='mx-1 p-1'>
                 {children}
             </div>
-            <div className='border border-solid rounded-md flex p-1'>
+            <div className='border border-solid border-black-olive rounded-md flex p-1'>
                 <input type={type} name={id} id={id} className={`bg-platinum px-1 max-w-[6rem] ${newBorderRight()} ${size && 'text-right'} appearance-none focus:outline-none`} />
                 <div className={`px-1 ${!size && 'invisible'}`}>
                     px
