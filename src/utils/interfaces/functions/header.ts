98f4e3bd4c7ede3d737e79b25d70ce480e35c968
Stablest <marlonlocci@hotmail.com>
2023-09-15 16:16:25 -0300

    refactor: Extracted findHeaderFileName from ImageProperties component.

---
+++ src/utils/interfaces/functions/header.ts
@@ -0,0 +1,13 @@
+function findHeaderFileName(headers: Headers, format? : boolean) {
+    let fileName = 'finer_img'
+    console.log([...headers.entries()])
+    const content = headers.get('content-disposition')
+    if (!content)
+        return fileName
+    fileName = content.split('filename=')[1].replaceAll('"', '')
+    if (!format)
+        fileName = fileName.split('.')[0]
+    return fileName
+}
+
+export { findHeaderFileName }
